59fae35c92b91e1f1102c8cfe88d21450208d961
“DavidAmyot” <davidamyot@me.com>
2020-03-05 09:55:34 -0500

    Added form, residential, corporate, 404, tos, privacy pages, all working

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get '/residential' => "pages#residential"
-  get 'pages/corporate'
-  get '/submissionform' => "pages#submissionform"
-  get 'pages/privacy'
-  get 'pages/404'
-  get 'pages/tos' 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   get '/index' => "pages#index"
   root 'pages#index'
+
+  get '/residential' => "pages#residential"
+  get '/corporate' => "pages#corporate"
+  get '/submissionform' => "pages#submissionform"
+  get '/privacy' => "pages#privacy"
+  get '/404' => "pages#404"
+  get '/tos'  => "pages#tos"
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
