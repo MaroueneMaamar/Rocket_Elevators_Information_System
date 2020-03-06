5c58b0177acc7f07806e951919d0b41adce58f90
“DavidAmyot” <davidamyot@me.com>
2020-03-06 12:16:13 -0500

    Safety push before deleting admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_201608) do
+ActiveRecord::Schema.define(version: 2020_03_06_165959) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 2020_03_05_201608) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.boolean "superadmin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
