ea5a251038bf0b289942e4ae6796120efc3e96ae
alexgreyer18 <alexander9708@hotmail.com>
2020-03-12 11:37:57 -0400

    Admin role created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
     def user_params
-        params.require(:user).permit(:email, :password, :password_confirmation, :remember_me)
+        params.require(:user).permit(:email, :password, :password_confirmation, :remember_me, :role)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  rolify
+
+  has_many :posts
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
