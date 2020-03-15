01ee813a1ed95b7103f555fb94a2bf1a25779ef8
MaroueneMaamar <maamarmarouene@gmail.com>
2020-03-15 11:30:39 -0400

    development.rb modified for deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,5 +64,9 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.hosts << "www.rocketelevator.ca"
+
+  config.hosts << "rocketelevator.ca"
   
 end
