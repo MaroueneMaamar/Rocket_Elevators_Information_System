b5b828545607f55d260971d46a56077dfb4816f4
“DavidAmyot” <davidamyot@me.com>
2020-03-17 12:19:39 -0400

    Changed something for roles since they don't work

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :customers
   has_many :buildings
 
-  belongs_to :role, :optional => true
+  belongs_to :role
   rolify
 
   after_create :assign_default_role
